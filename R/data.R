ff8f302dc5a38251869350bcb60e2ecc6b99214b
Florian Tanner <fctanner@pm.me>
2025-11-15 20:25:46 +0100

    feat: read raw trait data and save in data

---
+++ R/data.R
@@ -0,0 +1,4 @@
+#' `list` of raw trait data from all experiments,
+#'
+#' Generated with [read_raw_traits()]
+"raw_traits"
---
+++ data-raw/generate_data.R
@@ -0,0 +1,3 @@
+raw_traits <- read_raw_traits()
+
+usethis::use_data(raw_traits, overwrite = TRUE)
