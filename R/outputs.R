ef6ac62058722ab444a499528446ba32b1292240
Florian Tanner <fctanner@pm.me>
2026-01-10 18:30:34 +0100

    feat: save patchwork plot of scores (histograms and dotplots)

--- R/outputs.R
+++ R/outputs.R
@@ -27,30 +27,44 @@ make_score_table <- function(scores = scores) {
 #'
 #' @param scores `tibble` return value of [read_scores()]
 #' @param experiment_subset `character(1L)` one of `scores$subset`
+#' @param add_fill_legend_when_only_infected `logical(1L)` Should a color fill
+#'  legend be added to the plot if only Infected plants are included.
 #'
 #' @return `gg` object
-make_score_histogram <- function(scores = scores, experiment_subset){
+make_score_histogram <- function(
+    scores = scores,
+    experiment_subset,
+    add_fill_legend_when_only_infected = TRUE
+){
 
   color_palette <- paletteer::paletteer_d(`"basetheme::clean"`, 5)
   color_palette_infection <- color_palette[1:2]
 
   p_title <- paste0(experiment_subset, ": DI count")
 
+  data_subset <- scores |> dplyr::filter(subset == experiment_subset)
+  has_only_infected <- all(unique(data_subset$treatment) == "Infected")
+
   p <- ggplot2::ggplot() +
     ggplot2::geom_vline(xintercept = 40, linetype = 2, color = "grey60") +
     ggplot2::geom_vline(xintercept = 80, linetype = 2, color = "grey60") +
-    ggplot2::geom_histogram(data = scores |> dplyr::filter(subset == experiment_subset),
+    ggplot2::geom_histogram(data = data_subset,
                             ggplot2::aes(di, fill = treatment, color = treatment),
-                   alpha = 0.5, position = "identity") +
+                            alpha = 0.5, position = "identity") +
     ggplot2::scale_color_manual(values = c("Fungicide" = color_palette_infection[1], "Infected" = color_palette_infection[2])) +
     ggplot2::scale_fill_manual(values = c("Fungicide" = color_palette_infection[1], "Infected" = color_palette_infection[2])) +
     ggplot2::scale_x_continuous(breaks = c(0,20,40,60,80,100)) +
     ggplot2::labs(x = "Disease Index", subtitle = p_title) +
     ggplot2::theme_bw() +
     ggplot2::theme(panel.grid = ggplot2::element_blank(),
-          legend.title = ggplot2::element_blank(),
-          legend.position = "bottom",
-          axis.title.y = ggplot2::element_blank())
+                   legend.title = ggplot2::element_blank(),
+                   legend.position = "bottom",
+                   axis.title.y = ggplot2::element_blank())
+
+  if (!add_fill_legend_when_only_infected && has_only_infected) {
+    p <- p +
+      ggplot2::guides(colour = "none", fill = "none")
+  }
   return(p)
 }
 
@@ -69,14 +83,14 @@ make_control_genotypes_score_dotplot <- function(scores = scores){
   p <- scores |>
     dplyr::filter(.data$genotype_id %in% .env$control_genotypes) |>
     dplyr::mutate(grouping_var = stringr::str_c(subset, ": ", substr(.data$treatment, 1,3), "."),
-           grouping_var = factor(grouping_var, levels = c("2022 Subset: Inf.",
-                                                          "2022 Subset: Fun.",
-                                                          "2021 Subset: Inf.",
-                                                          "2021 Subset: Fun.",
-                                                          "2022 Main: Inf.",
-                                                          "2021 Main: Inf.",
-                                                          "2020 Main: Inf."
-           ))) |>
+                  grouping_var = factor(grouping_var, levels = c("2022 Subset: Inf.",
+                                                                 "2022 Subset: Fun.",
+                                                                 "2021 Subset: Inf.",
+                                                                 "2021 Subset: Fun.",
+                                                                 "2022 Main: Inf.",
+                                                                 "2021 Main: Inf.",
+                                                                 "2020 Main: Inf."
+                  ))) |>
     ggplot2::ggplot(ggplot2::aes(x = di, y = grouping_var, color= genotype_id, shape = genotype_id)) +
     ggplot2::geom_jitter(height = 0.2, width = 0, alpha = 0.7) +
     ggplot2::scale_color_manual(values = color_palette_genotypes) +
@@ -84,8 +98,8 @@ make_control_genotypes_score_dotplot <- function(scores = scores){
     ggplot2::labs(x = "Disease Index", subtitle= "Control genotypes: DI per pot") +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.title = ggplot2::element_blank(),
-          legend.position = "bottom",
-          axis.title.y = ggplot2::element_blank())
+                   legend.position = "bottom",
+                   axis.title.y = ggplot2::element_blank())
 
   return(p)
 }
@@ -119,10 +133,42 @@ make_scoring_repeatability_table <- function() {
 #' Builds table for distribution of genotypes in each experiment
 #'
 #' @return reactable Object
-make_genotype_distribution_table <- function() {
+make_genotype_distribution_table <- function(scores = scores) {
   summarise_genotype_distribution() |>
     tidyr::pivot_wider(id_cols = subset, names_from = type, values_from = n) |>
     dplyr::rename("Subset" = subset) |>
     reactable::reactable()
 }
 
+
+#' Wraps the plots displaying the scores into a single plot
+#'
+#' @return `patchwork` Object
+save_score_patchwork_plot <- function() {
+
+  subsets <- unique(scores$subset)
+  control_genotypes_score_dotplot <- make_control_genotypes_score_dotplot(scores)
+
+  score_histograms <- subsets |>
+    purrr::map(\(s) {
+      p <- make_score_histogram(scores, s, add_fill_legend_when_only_infected = FALSE)
+      print(p)
+      p
+    })
+
+  plots <- c(list(control_genotypes_score_dotplot), score_histograms)
+  p <- patchwork::wrap_plots(plots) +
+    patchwork::plot_layout(
+      guides = "collect",
+      design = "
+        AABB
+        CCDD
+        EEFF
+      "
+    ) &
+    ggplot2::theme(legend.position = "bottom")
+
+  ggplot2::ggsave(filename = "out/scores_patchwork.pdf", plot = p,
+                  units = "cm", width =12, height = 10, limitsize = F,
+                  device = "pdf", scale = 1.5)
+}
